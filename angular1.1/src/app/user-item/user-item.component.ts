4eeeba2560ee41bd9d13903e86f5c266418726e0
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2024-01-06 11:37:11 +0000

    es 1.1

---
+++ angular1.1/src/app/user-item/user-item.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+export class UserItemComponent {
+name : string;
+constructor(){
+ this.name='felipe'; 
+}
+
+}
