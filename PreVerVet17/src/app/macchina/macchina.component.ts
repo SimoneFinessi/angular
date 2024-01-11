4012d7c097a6682f6325772cead95e00da2794d2
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2024-01-11 11:27:00 +0000

    punto 1

---
+++ PreVerVet17/src/app/macchina/macchina.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-macchina',
+  standalone: true,
+  imports: [],
+  templateUrl: './macchina.component.html',
+  styleUrl: './macchina.component.css'
+})
+export class MacchinaComponent {
+
+}
