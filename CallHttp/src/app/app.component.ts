73fc4effcd8212515515426a92ef32737da1ec5f
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2024-04-15 10:47:43 +0000

    chiamata http

--- CallHttp/src/app/app.component.ts
+++ CallHttp/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CallHttp';
+  citofono! :Observable<any>;
+  myData_Save:any={};
+  constructor(private http:HttpClient){
+    
+    this.citofono=this.http.get("https://jsonplaceholder.typicode.com/posts/42");
+    this.citofono.subscribe(this.faiQualcosa)
+  }
+  faiQualcosa=(myData:any)=>{
+    this.myData_Save=myData;
+  }
 }
