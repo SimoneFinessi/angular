9f5aa9f07babefcd9b78f71950e9ba40aac4b7f3
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2024-04-18 12:46:53 +0000

    finito 1

--- CallHttp/src/app/app.component.ts
+++ CallHttp/src/app/app.component.ts
@@ -10,15 +10,28 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   data: Object;
   loading: boolean;
-  o :Observable<Object>;
-  constructor(public http:HttpClient){}
-  makeRequest(): void{
-    this.loading=true;
-    this.o=this.http.get("https://jsonplaceholder.typicode.com/posts/42");
+  o: Observable<Object>;
+  obsP: Observable<any>;
+  constructor(public http: HttpClient) { }
+  makeRequest(): void {
+    this.loading = true;
+    this.o = this.http.get("https://jsonplaceholder.typicode.com/posts/42");
     this.o.subscribe(this.getData);
   }
-  getData=(d:object)=>{
-    this.data=d;
-    this.loading=false;
+  getData = (d: object) => {
+    this.data = d;
+    this.loading = false;
+  }
+
+  makeCompactPost(): void {
+    this.loading = true;
+
+    this.obsP = this.http.post('https://jsonplaceholder.typicode.com/posts', JSON.stringify({ body: 'bar', title: 'foo', userId: 1 }))
+
+    this.obsP.subscribe(this.getdatapost)
+  }
+  getdatapost = data => {
+    this.data = data;
+    this.loading = false;
   }
 }
